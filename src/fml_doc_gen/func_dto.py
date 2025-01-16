4df2423f4bbb9cfeb70cb60eecb457cc16fcfdf9
mikem2m <michael08ace@gmail.com>
2025-01-15 23:10:50 -0800

    Fix 2nd test

--- src/fml_doc_gen/func_dto.py
+++ src/fml_doc_gen/func_dto.py
@@ -1,7 +1,7 @@
 from typing import List, Optional, Tuple
 
 class FunctionDTO:
-    def __init__(self, name: str, output: Optional[str] = None, inputs: Optional[List[Tuple[str, str]]] = []):
+    def __init__(self, name: Optional[str] = "", output: Optional[str] = None, inputs: Optional[List[Tuple[str, str]]] = []):
         self.name = name
         self.output_type = output
         self.inputs = [(name, input_type) for (name, input_type) in inputs]
